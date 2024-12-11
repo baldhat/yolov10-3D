e3c60057b219ba5056ffb6d0caa43316c9e84b14
baldhat <michel.jonathan@proton.me>
2024-12-11 14:10:56 +0100

    Fix self dist bug

--- ultralytics/data/datasets/kitti.py
+++ ultralytics/data/datasets/kitti.py
@@ -47,6 +47,8 @@ class KITTIDataset(data.Dataset):
         root_dir = pathlib.Path(image_file_path).parent.parent
         split_dir = image_file_path
         self.idx_list = [x.strip() for x in open(split_dir).readlines()]
+        if args.overfit:
+            self.idx_list = self.idx_list[:64]
 
         # path configuration
         self.data_dir = os.path.join(root_dir, 'testing' if self.mode == 'test' else 'training')
