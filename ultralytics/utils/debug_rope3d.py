df783f36de9c53db88c69625ee06c115ad35c46f
baldhat <michel.jonathan@proton.me>
2025-01-25 09:40:42 +0100

    3000f

--- ultralytics/utils/debug_rope3d.py
+++ ultralytics/utils/debug_rope3d.py
@@ -17,7 +17,7 @@ def plot_bev(target, img, color=(0, 255, 0)):
 
 
 
-pred_directory = Path("/home/stud/mijo/dev/yolov10-3D/runs/detect/val15/preds")
+pred_directory = Path("/home/stud/mijo/dev/yolov10-3D/runs/detect/val2/preds")
 target_directory = Path("/home/stud/mijo/storage/group/deepscenario/rope3d/val/label_2/")
 
 file_name = "150696_sj8fas23e152d20211201air_420_1637290455_1637292954_455_obstacle.txt"
