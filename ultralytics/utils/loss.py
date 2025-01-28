bd89f47d39561ce8ba52086f1a5627e68a5f3ed5
baldhat <michel.jonathan@proton.me>
2025-01-28 06:30:29 +0100

    add n model

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -911,8 +911,8 @@ class DDDetectionLoss:
         return loss.sum() * batch_size, loss
 
     def plot_assignments(self, batch, targets_2d, fg_mask, pred_bboxes, stride_tensor, targets_3d,  pred_kps, gt_kps, mask_gt):
-        #self.debug_show_assigned_targets2d(batch, targets_2d, fg_mask, pred_bboxes, stride_tensor)
-        #self.debug_show_assigned_targets3d(batch, targets_3d, fg_mask, pred_kps, gt_kps, mask_gt)
+        self.debug_show_assigned_targets2d(batch, targets_2d, fg_mask, pred_bboxes, stride_tensor)
+        self.debug_show_assigned_targets3d(batch, targets_3d, fg_mask, pred_kps, gt_kps, mask_gt)
         self.debug_show_pred_bevs(pred_kps, gt_kps, fg_mask, mask_gt, stride_tensor)
 
     def compute_loss_weights(self, current_loss):
@@ -1012,7 +1012,8 @@ class DDDetectionLoss:
 
             ax.imshow(img)
             ax.axis("off")
-        plt.show()
+        #plt.show()
+        plt.savefig("2d_assignments.png")
         print()
 
     def debug_show_assigned_targets3d(self, batch, targets_3d, fg_mask, pred_kps, gt_kps, mask_gt):
@@ -1048,7 +1049,8 @@ class DDDetectionLoss:
 
             ax.imshow(img)
             ax.axis("off")
-        plt.show()
+        #plt.show()
+        plt.savefig("3d_assignments.png")
         print()
 
     def debug_show_pred_bevs(self, pred_kps, gt_kps, fg_mask, mask_gt, stride_tensor):
