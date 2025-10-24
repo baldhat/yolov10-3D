b7facc222c9262dffec7110694ffc42ff0c7fa39
baldhat <michel.jonathan@proton.me>
2025-10-24 20:22:29 +0200

    Update BEV visualizatinos

--- ultralytics/utils/qualitative_candidates.py
+++ ultralytics/utils/qualitative_candidates.py
@@ -7,7 +7,7 @@ import math
 import operator
 
 import matplotlib.pyplot as plt
-from matplotlib.patches import Circle, Polygon
+from matplotlib.patches import Circle, Polygon, Wedge
 
 from ultralytics.data.datasets.kitti_utils import Object3d, Calibration
 from ultralytics.utils.metrics import box_iou
@@ -16,6 +16,11 @@ from scipy.spatial.transform import Rotation
 from scipy.optimize import linear_sum_assignment
 
 plotter = KITTIVisualizer()
+
+map = plt.colormaps.get_cmap("Set1")
+gt_color = map(2)
+our_color = map(1)
+base_color = map(0)
 
 class Detection3d:
     def __init__(self, line):
@@ -66,7 +71,7 @@ def load_calib(path):
     return Calibration(str(path))
 
 def load_image(path):
-    return cv.imread(path)
+    return cv.imread(str(path))
 
 def plot_labels(img, gts: [Object3d], calib, color):
     for object in gts:
@@ -96,7 +101,8 @@ def plot_dets(img, dets, calib, color):
                                         dimensions, bbox2d, cls),
                             calib.P2, bbox2d=False)
 
-def plot_bev(gts, base_dets, filename):
+def plot_bev(gts, base_dets, our_dets, filename, fov=60):
+    print(fov)
     plt.clf()
 
     def get_rotated_rectangle_points(center, size, angle_degrees):
@@ -126,38 +132,65 @@ def plot_bev(gts, base_dets, filename):
                         figsize=(24, 12), gridspec_kw={'wspace': 0, 'hspace': 0}, constrained_layout=True)
 
     R = 60
-    ax.set_xlim(-R, R)
-    ax.set_ylim(0, R)
+    border = 6
+    ax.set_xlim(-R - border, R + border)
+    ax.set_ylim(-border, R + border)
     ax.set_aspect(1.0)
     ax.set_xticks([])
     ax.set_yticks([])
-    ax.set_facecolor('black')
+    ax.set_facecolor((0.9, 0.9, 0.9))
+    
 
-    for theta in np.linspace(0, np.pi, 7):
-        xs, ys = [R * np.cos(theta), 0], [R * np.sin(theta), 0]
-        ax.plot(xs, ys, linewidth=2, color=(1, 1, 1), zorder=1)
+    # for theta in np.linspace(0, np.pi, 7):
+    #     xs, ys = [R * np.cos(theta), 0], [R * np.sin(theta), 0]
+    #     ax.plot(xs, ys, linewidth=2, color=(1, 1, 1), zorder=1)
 
-    for radius in np.linspace(0, R, 5):
+    for radius, c_color in zip(np.linspace(R, 0, 7), np.linspace(0.8, 0.35, 7)):
+        x = np.sin(np.deg2rad(fov / 2)) * (radius - 1.5)
+        y = np.cos(np.deg2rad(fov / 2)) * (radius - 1.5)
+        ax.text(x + 1.3, y - 1.2, str(int(radius)) + "m", rotation=-(5 + fov/2), fontsize=20, color=(0.15, 0.15, 1))
         if radius == 0:
             continue
-        circle = Circle((0, 0), radius, edgecolor=(1, 1, 1), linewidth=2, fill=False, zorder=1)
+        circle = Circle((0, 0), radius, color=(c_color, c_color, c_color), linewidth=2, fill=True, zorder=1)
         ax.add_artist(circle)
+        
+        
+        0, 252, 239
+    lightblue = (0, 252/255.0, 239/255.0, 32/255.0)
+    wedge = Wedge((0, 0), R, -fov/2 + 90, fov/2 + 90, color=lightblue)
+    ax.add_artist(wedge)
 
-    for object in gts:
+    for j, object in enumerate(gts):            
         dimensions = np.array([object.l, object.w])
         translation = object.pos[[0, 2]]
         ry = -object.ry
 
         corners = get_rotated_rectangle_points(translation, dimensions, ry * 180 / np.pi)
-        ax.add_artist(Polygon(corners, closed=True, fill=True, edgecolor='g', facecolor="g", zorder=3))
+        art = ax.add_artist(Polygon(corners, closed=True, fill=False, edgecolor=gt_color, facecolor=gt_color, zorder=3, linewidth=5))
+        if j == 0:
+            art.set_label("Ground Truth")
         
-    for object in base_dets:
+    for j, object in enumerate(base_dets):
         dimensions = object.dimensions[::-1][:2]
         translation = object.location[[0, 2]]
         ry = -object.ry
 
         corners = get_rotated_rectangle_points(translation, dimensions, ry * 180 / np.pi)
-        ax.add_artist(Polygon(corners, closed=True, fill=True, edgecolor='r', facecolor="r", zorder=3))
+        art = ax.add_artist(Polygon(corners, closed=True, fill=False, edgecolor=base_color, facecolor=base_color, zorder=3, linewidth=5))
+        if j == 0:
+            art.set_label("Baseline")
+        
+    for j, object in enumerate(our_dets):
+        dimensions = object.dimensions[::-1][:2]
+        translation = object.location[[0, 2]]
+        ry = -object.ry
+
+        corners = get_rotated_rectangle_points(translation, dimensions, ry * 180 / np.pi)
+        art = ax.add_artist(Polygon(corners, closed=True, fill=False, edgecolor=our_color, facecolor=our_color, zorder=3, linewidth=5))
+        if j == 0:
+            art.set_label("Ours")
+    
+    ax.legend()
 
     plt.savefig(filename, bbox_inches="tight", format="svg")
     print(filename)
@@ -178,96 +211,99 @@ def plot_all(img, gts, our_dets, base_dets, calib, out_path):
     cv.imwrite(base_name, (base_img*255.0).astype(np.uint8))
     print(base_name)
     
-    plot_bev(gts, base_dets, out_path.replace(".png", "_base_bev.svg"))
-    plot_bev(gts, our_dets, out_path.replace(".png", "_ours_bev.svg"))
+    plot_bev(gts, base_dets, our_dets, out_path.replace(".png", "_bev.svg"), np.rad2deg(2*np.arctan2(base_img.shape[1], 2* calib.fu)))
     
-
-val_files = Path("/storage/group/deepscenario/KITTI/ImageSets/val.txt")
-
-import sys
-if len(sys.argv) >= 2:
-    print(sys.argv)
-    base_path = Path(sys.argv[1])
-    ours_path = Path(sys.argv[2])
-    ours_name = str(ours_path).split("/")[-1]
-    ours_name = str(base_path).split("/")[-1]
-else:
-    base_name = "yolov10-3D_rope3d_baselineNoMixup_60_n_17"
-    ours_name = "yolov10-3D_rope3d_ours_n_60_2"
-    base_path = Path("/storage/group/deepscenario/for_jonathan/" + base_name)
-    ours_path = Path("/storage/group/deepscenario/for_jonathan/" + ours_name)
-
-output_path = Path("/storage/group/deepscenario/jonathan_for_johannes/") / ours_name
-if not os.path.exists(output_path):
-    os.mkdir(output_path)
-
-gt_path = Path("/storage/group/deepscenario/KITTI/training/label_2/")
-
-counter = 0
-
-scores = {}
-
-for fn in open(val_files, "r").readlines():
-    filename = fn.strip() + ".txt"
-    plot = False
-    # load dets and gts
-    base_dets = load_dets(base_path / "preds" / filename)
-    our_dets = load_dets(ours_path / "preds" / filename)
-    gts = load_labels(gt_path / filename)
+if __name__=='__main__':
+    test_plot = False
     
-    # filter dets by score and class
-    base_dets_ = filter_(base_dets)
-    our_dets_ = filter_(our_dets)
-    if len(our_dets_) == 0:
-        continue
-    
-    # associate dets to gts
-    base_gts, base_dets, base_false_positives = associate(gts, base_dets_)
-    our_gts, our_dets, our_false_positives = associate(gts, our_dets_)
-    
-    improvement_counter = 0
-    # check missing detections
-    if len(base_false_positives) > len(our_false_positives):
-        pass
-        #print("False positive")
-        #improvement_counter += (len(base_false_positives) - len(our_false_positives))
-    
-    # calculate position and rotation errors
-    base_pos_errors, base_rot_errors = calculate_errors(base_gts, base_dets)
-    our_pos_errors, our_rot_errors = calculate_errors(our_gts, our_dets)
-    
-    # print where the base errors are significantly larger than our errors
-    for j, our_gt in enumerate(our_gts):
-        found = False
-        for i, base_gt in enumerate(base_gts):
-            found = True
-            if not equals(base_gt, our_gt):
-                continue
-            
-            diff = abs(base_pos_errors[i] - our_pos_errors[j])
-            if diff > 1.5 and diff < 15:
-                print(f"Better Location! Base: {base_dets[i].location}, Ours: {our_dets[j].location}")
-                improvement_counter += 1 #math.ceil(base_pos_errors[i] - our_pos_errors[j] - 5)
-                
-            if np.abs(base_rot_errors[i] - our_rot_errors[j]) > 1:
-                #print(f"Better Rotation! Base: {base_dets[i].ry}, Ours: {our_dets[j].ry}")
-                #plot = True
-                pass
-        if not found:
-            pass
-            #print("We detected more objects")
-            #improvement_counter += 1
-                
-    if improvement_counter > 3:
-        print(filename)
-        img_name = filename.replace("txt", "png")
-        img = load_image(gt_path / ".." / "image_2" / img_name).astype(np.float32) / 255.0
-        calib = load_calib(gt_path / ".." / "calib" / filename)
-        out_path = output_path / img_name
-        plot_all(img, gts, our_dets, base_dets, calib, str(out_path))
-        counter += 1
-        scores[filename] = improvement_counter
+    val_files = Path("/storage/group/deepscenario/KITTI/ImageSets/val.txt")
+
+    import sys
+    if len(sys.argv) >= 2:
+        print(sys.argv)
+        base_path = Path(sys.argv[1])
+        ours_path = Path(sys.argv[2])
+        ours_name = str(ours_path).split("/")[-1]
+        ours_name = str(base_path).split("/")[-1]
+    else:
+        base_name = "yolov10-3D_rope3d_baselineNoMixup_60_n_17"
+        ours_name = "yolov10-3D_rope3d_ours_n_60_2"
+        base_path = Path("/storage/group/deepscenario/for_jonathan/" + base_name)
+        ours_path = Path("/storage/group/deepscenario/for_jonathan/" + ours_name)
+
+    output_path = Path("/storage/group/deepscenario/jonathan_for_johannes/") / ours_name
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    gt_path = Path("/storage/group/deepscenario/KITTI/training/label_2/")
+
+    counter = 0
+
+    scores = {}
+
+    for ci, fn in enumerate(open(val_files, "r").readlines()):
+        if test_plot and ci > 3:
+            break
+        filename = fn.strip() + ".txt"
+        plot = False
+        # load dets and gts
+        base_dets = load_dets(base_path / "preds" / filename)
+        our_dets = load_dets(ours_path / "preds" / filename)
+        gts = load_labels(gt_path / filename)
         
-print(f"\n\nFound {counter} candidates overall.")
-print("Top Ten:")
-print("\n".join([it[0] +": " + str(it[1]) for it in list(reversed(sorted(scores.items(), key=operator.itemgetter(1))))[:10]]))
+        # filter dets by score and class
+        base_dets_ = filter_(base_dets)
+        our_dets_ = filter_(our_dets)
+        if len(our_dets_) == 0:
+            continue
+        
+        # associate dets to gts
+        base_gts, base_dets, base_false_positives = associate(gts, base_dets_)
+        our_gts, our_dets, our_false_positives = associate(gts, our_dets_)
+        
+        improvement_counter = 0
+        # check missing detections
+        if len(base_false_positives) > len(our_false_positives):
+            pass
+            #print("False positive")
+            #improvement_counter += (len(base_false_positives) - len(our_false_positives))
+        
+        # calculate position and rotation errors
+        base_pos_errors, base_rot_errors = calculate_errors(base_gts, base_dets)
+        our_pos_errors, our_rot_errors = calculate_errors(our_gts, our_dets)
+        
+        # print where the base errors are significantly larger than our errors
+        for j, our_gt in enumerate(our_gts):
+            found = False
+            for i, base_gt in enumerate(base_gts):
+                found = True
+                if not equals(base_gt, our_gt):
+                    continue
+                
+                diff = abs(base_pos_errors[i] - our_pos_errors[j])
+                if diff > 1.5 and diff < 15:
+                    print(f"Better Location! Base: {base_dets[i].location}, Ours: {our_dets[j].location}")
+                    improvement_counter += 1 #math.ceil(base_pos_errors[i] - our_pos_errors[j] - 5)
+                    
+                if np.abs(base_rot_errors[i] - our_rot_errors[j]) > 1:
+                    #print(f"Better Rotation! Base: {base_dets[i].ry}, Ours: {our_dets[j].ry}")
+                    #plot = True
+                    pass
+            if not found:
+                pass
+                #print("We detected more objects")
+                #improvement_counter += 1
+                    
+        if improvement_counter > 3 or test_plot:
+            print(filename)
+            img_name = filename.replace("txt", "png")
+            img = load_image(gt_path / ".." / "image_2" / img_name).astype(np.float32) / 255.0
+            calib = load_calib(gt_path / ".." / "calib" / filename)
+            out_path = output_path / img_name
+            plot_all(img, gts, our_dets, base_dets, calib, str(out_path))
+            counter += 1
+            scores[filename] = improvement_counter
+            
+    print(f"\n\nFound {counter} candidates overall.")
+    print("Top Ten:")
+    print("\n".join([it[0] +": " + str(it[1]) for it in list(reversed(sorted(scores.items(), key=operator.itemgetter(1))))[:10]]))
