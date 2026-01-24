fbe9600214c85ceccb01e3ea1ce8c44f6dd26374
baldhat <michel.jonathan@proton.me>
2026-01-24 14:19:09 +0100

    correct eval

--- ultralytics/data/datasets/kitti.py
+++ ultralytics/data/datasets/kitti.py
@@ -5,7 +5,7 @@ import cv2
 import torch
 import pathlib
 from ultralytics.data.datasets.decode_helper import  *
-# from ultralytics.data.datasets.kitti_eval import eval_from_scrach
+from ultralytics.data.datasets.kitti_eval import eval_from_scrach
 import subprocess
 from pathlib import Path
 
@@ -518,17 +518,22 @@ class KITTIDataset(data.Dataset):
 
     def get_stats(self, results, save_dir):
         self.save_results(results, output_dir=save_dir)
-        self.save_results(results, output_dir=str(save_dir), epoch=self.save_counter)
-        command = f"ultralytics/data/datasets/evaluate_object_3d_offline_ap40 {self.label_dir} {os.path.join(save_dir, 'preds')}"
-        print("Running command: " + command)
-        lines = subprocess.check_output(command, shell= True, text= True, env={})
-        print("Result: " + lines)
-        result = 0
-        for line in lines.split("\n"):
-            if line.startswith("car_detection_3d"):
-                result = float(line.split(" ")[3])
-        self.last_result = result
-        return self.last_result
+        # self.save_results(results, output_dir=str(save_dir), epoch=self.save_counter)
+        # command = f"ultralytics/data/datasets/evaluate_object_3d_offline_ap40 {self.label_dir} {os.path.join(save_dir, 'preds')}"
+        # print("Running command: " + command)
+        # lines = subprocess.check_output(command, shell= True, text= True, env={})
+        # print("Result: " + lines)
+        # result = 0
+        # for line in lines.split("\n"):
+        #     if line.startswith("car_detection_3d"):
+        #         result = float(line.split(" ")[3])
+        # self.last_result = result
+        # return self.last_result
+        result = eval_from_scrach(
+            self.label_dir,
+            os.path.join(save_dir, 'preds'),
+            ap_mode=40)
+        return result["3d@0.70"][1]
 
     def save_results(self, results, output_dir='./outputs', epoch=None):
         output_dir = str(os.path.join(output_dir, 'preds'))
