f01315511bb4d990511b33f486cded6484b20fff
baldhat <michel.jonathan@proton.me>
2026-01-25 10:30:24 +0100

    fix not increasing save counter

--- ultralytics/data/datasets/kitti.py
+++ ultralytics/data/datasets/kitti.py
@@ -517,6 +517,7 @@ class KITTIDataset(data.Dataset):
         return dst_img
 
     def get_stats(self, results, save_dir):
+        self.save_counter += 1
         self.save_results(results, output_dir=save_dir)
         if self.args.fast_eval:
             try:
